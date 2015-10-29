ce6682a9d0472db313fe30fcabd550816c2ecfdb
Alex <asrocha92@gmail.com>
2015-10-29 07:44:07 -0200

    Classe DaoUsuario criada que implementa CrudDao. Método inserir pronto.

---
+++ src/main/java/trab4bim/Dao/DaoUsuario.java
@@ -0,0 +1,52 @@
+package trab4bim.Dao;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
+
+import javax.swing.JOptionPane;
+
+import trab4bim.classes.Usuario;
+
+public class DaoUsuario implements CrudDao<Usuario>{
+
+	private PreparedStatement ps = null;
+	private Statement st = null;
+	private ResultSet rs = null;
+	private Usuario u = null;
+	private List<Usuario> lista = null;
+	
+	public void inserir(Usuario u) {
+		try {
+			ps = Conexao.con.prepareStatement("INSERT INTO USUARIO (ID_C, CLIENTE, SENHA) VALUES (?, ?, ?)");
+			ps.setInt(1, u.getIdCliente());
+			ps.setString(2, u.getCliente());
+			ps.setString(3, u.getSenha());
+			ps.executeUpdate();
+			ps.close();
+			JOptionPane.showMessageDialog(null, "Usuário cadastrado com\n"
+													 + "Sucesso");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void atualizar(Usuario u) {
+		
+	}
+
+	public void deletar(int id_u) {
+		
+	}
+
+	public Usuario buscarUm(int id_u) {
+		return null;
+	}
+
+	public List<Usuario> listar() {
+		return null;
+	}
+
+}
