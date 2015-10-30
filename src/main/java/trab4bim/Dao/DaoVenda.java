49900d1fadc1acdd92c3423536d419a22d3711a8
Alex <asrocha92@gmail.com>
2015-10-30 15:11:55 -0200

    Método atualizar e deletar da classe DaoVenda pronto.

--- src/main/java/trab4bim/Dao/DaoVenda.java
+++ src/main/java/trab4bim/Dao/DaoVenda.java
@@ -3,6 +3,7 @@ package trab4bim.Dao;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.List;
 
@@ -14,6 +15,9 @@ import trab4bim.classes.Venda;
 /**
  * 
  * @author Alex Santos Rocha, 29/10/2015 - 19:23:06
+ *
+ *	Comentario: Classe DaoVenda implementa DaoCrud e 
+ *	através implementa métodos que se relacionem com um banco de dados.
  *
  */
 public class DaoVenda implements CrudDao<Venda> {
@@ -45,14 +49,39 @@ public class DaoVenda implements CrudDao<Venda> {
 		}
 	}
 
-	public void atualizar(Venda tipo) {
-		// TODO Auto-generated method stub
-
+	public void atualizar(Venda vd) {
+		try {
+			ps = con.prepareStatement("UPDATE VENDA SET ID_C = ?, CLIENTE = ?, COD_P = ?,"
+					+ " PRODUTO = ?, VTOTAL = ?, VPAGAMENTO = ?, TROCO = ?, DATA = ?,"
+					+ " HORA = ? WHERE COD_V ="+vd.getCod_v());
+			ps.setInt(1, vd.getId_c());
+			ps.setString(2, vd.getCliente());
+			ps.setInt(3, vd.getCod_p());
+			ps.setString(4, vd.getProduto());
+			ps.setBigDecimal(5, vd.getvTotal());
+			ps.setBigDecimal(6, vd.getvPago());
+			ps.setBigDecimal(7, vd.getTroco());
+			ps.setDate(8, vd.getData());
+			ps.setTime(9, vd.getHora());
+			ps.executeUpdate();
+			ps.close();
+			JOptionPane.showMessageDialog(null, "Venda Efetuada com sucesso.");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
-	public void deletar(int tipo) {
-		// TODO Auto-generated method stub
-
+	public void deletar(int cod_v) {
+		try {
+			ps = con
+					.prepareStatement("DELETE FROM VENDA WHERE COD_V =" + cod_v);
+			int res = ps.executeUpdate();
+			ps.close();
+			JOptionPane.showMessageDialog(null, res
+					+ ", Requisição de venda excluida com sucesso");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 
 	public Venda buscarUm(int id) {
